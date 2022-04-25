115bf38bf5f2059bf61261adb7685211703649de
stranothus <stranothus@gmail.com>
2022-04-25 13:29:41 -0500

    Make the email form submittable frontend

--- utils/footer/index.tsx
+++ utils/footer/index.tsx
@@ -1,17 +1,19 @@
 import Link from "next/link";
-import React from "react";
-import styles from "../../public/variables.module.scss";
-import TopZag from "../shapes/top/zag";
+import React, { ChangeEvent } from "react";
 import style from "./style.module.scss";
 
+const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
+
 interface FooterState {
-    inputValue: string
+    inputValue: string,
+    inputValid: boolean
 }
 
 export default class Footer extends React.Component {
     emailRef:React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
     state: FooterState = {
-        inputValue: ""
+        inputValue: "",
+        inputValid: false
     };
 
     constructor(props: {}) {
@@ -30,14 +32,17 @@ export default class Footer extends React.Component {
                     <h2>Student Resources</h2>
                     <h3>© Quinn Gibson</h3>
                 </div>
-                <div className={"input " + style.input}>
+                <form className={"input " + style.input} action="/api/email/sign-up" method="POST" encType="multipart/form-data">
                     <input onChange={this.checkValue} ref={this.emailRef} name="footer-email"></input>
                     <label htmlFor="footer-email" className={this.state.inputValue ? "inputted" : "empty"}>Get email updates</label>
-                </div>
+                    <button type="submit" style={{ visibility: this.state.inputValid ? "visible" : "hidden" }}>Sign up</button>
+                </form>
             </footer>
         );
     }
-    checkValue(): void {
+    checkValue(event: ChangeEvent<HTMLInputElement>): void {
+        event.preventDefault();
         this.setState({ inputValue: this.emailRef.current?.value || "" });
+        this.setState({ inputValid: emailRegex.test(this.emailRef.current?.value || "") });
     }
 }
